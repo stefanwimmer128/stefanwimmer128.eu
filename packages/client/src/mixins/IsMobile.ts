f94c38814be683c442c3c4cec969090d774aa457
Stefan Wimmer <info@stefanwimmer128.eu>
2019-06-07 20:23:06 +0200

    Streamline IsMobile mixin

--- packages/client/src/mixins/IsMobile.ts
+++ packages/client/src/mixins/IsMobile.ts
@@ -3,9 +3,32 @@ import {
     Component,
 } from "@vue-decorators/all";
 
+function computeIsMobile() {
+    return window.matchMedia("only screen and (max-width: 760px)").matches;
+}
+
+const isMobileUpdate = new Set<IsMobile>();
+function updateIsMobile() {
+    const isMobile = computeIsMobile();
+    for (const component of isMobileUpdate)
+        component.isMobile = isMobile;
+}
+
 @Component
 export default class IsMobile extends Vue {
-    get isMobile() {
-        return window.matchMedia("only screen and (max-width: 760px)").matches;
+    isMobile = computeIsMobile();
+    
+    mounted() {
+        if (isMobileUpdate.size === 0)
+            window.addEventListener("resize", updateIsMobile);
+        
+        isMobileUpdate.add(this);
+    }
+    
+    beforeDestroy() {
+        isMobileUpdate.delete(this);
+        
+        if (isMobileUpdate.size === 0)
+            window.removeEventListener("resize", updateIsMobile);
     }
 }
