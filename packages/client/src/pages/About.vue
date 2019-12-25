897a9d996d6bd0f1beb19921650050f9ecc4bac8
Stefan Wimmer <info@stefanwimmer128.eu>
2019-12-25 18:30:59 +0100

    Update About page

--- packages/client/src/pages/About.vue
+++ packages/client/src/pages/About.vue
@@ -15,11 +15,5 @@
 <template lang="pug">
     div
         h2 About me
-        p Hello, I'm Stefan Wimmer, I'm a hobby programmer at the age of 19.
-        p I started programming when I was 13 in 6th grade. Since then I learned numerous programming languages, including HTML - yes, I consider HTML a programming language -, PHP, JavaScript, Shell Script, Kotlin, Scala and Java.
-        p For 2 years now I started to work with Node.js and NPM - or now Yarn - and even published some own packages like 
-            code @vue-decorators/all
-            |  or 
-            code @stefanwimmer128/core
-            | .
+        p I'm a Web Developer (in apprenticeship) and anime/manga/cartoon/book-enthusiast.
 </template>
